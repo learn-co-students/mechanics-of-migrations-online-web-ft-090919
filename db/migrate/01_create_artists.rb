8658a29461392db43e1323cbeb7c5337bc3a7d77
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-10-16 10:44:32 -0500

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+   def change
+   end
+
+    
+  #   sql = <<-SQL
+  #   CREATE TABLE IF NOT EXISTS artists (
+  #   id INTEGER PRIMARY KEY,
+  #   name TEXT,
+  #   genre TEXT,
+  #   age INTEGER,
+  #   hometown TEXT
+  #   )
+  # SQL
+   
+  # ActiveRecord::Base.connection.execute(sql)
+
+
+end
